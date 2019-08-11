bb129bace6544725291b5b7b455b2370ea2a5734
fgshun <fgshun@gmail.com>
2019-08-11 21:40:02 +0900

    argument clinic on Python 3.8.0b3

--- clinic/cjholiday.c.h
+++ clinic/cjholiday.c.h
@@ -9,7 +9,7 @@ PyDoc_STRVAR(cjholiday_holiday_name__doc__,
 "get name of holiday from ymd or datetime.date.");
 
 #define CJHOLIDAY_HOLIDAY_NAME_METHODDEF    \
-    {"holiday_name", (PyCFunction)cjholiday_holiday_name, METH_FASTCALL|METH_KEYWORDS, cjholiday_holiday_name__doc__},
+    {"holiday_name", (PyCFunction)(void(*)(void))cjholiday_holiday_name, METH_FASTCALL|METH_KEYWORDS, cjholiday_holiday_name__doc__},
 
 static PyObject *
 cjholiday_holiday_name_impl(PyObject *module, PyObject *pyyear,
@@ -21,19 +21,44 @@ cjholiday_holiday_name(PyObject *module, PyObject *const *args, Py_ssize_t nargs
 {
     PyObject *return_value = NULL;
     static const char * const _keywords[] = {"year", "month", "day", "date", NULL};
-    static _PyArg_Parser _parser = {"|OOOO:holiday_name", _keywords, 0};
+    static _PyArg_Parser _parser = {NULL, _keywords, "holiday_name", 0};
+    PyObject *argsbuf[4];
+    Py_ssize_t noptargs = nargs + (kwnames ? PyTuple_GET_SIZE(kwnames) : 0) - 0;
     PyObject *pyyear = NULL;
     PyObject *pymonth = NULL;
     PyObject *pyday = NULL;
     PyObject *date = NULL;
 
-    if (!_PyArg_ParseStackAndKeywords(args, nargs, kwnames, &_parser,
-        &pyyear, &pymonth, &pyday, &date)) {
+    args = _PyArg_UnpackKeywords(args, nargs, NULL, kwnames, &_parser, 0, 4, 0, argsbuf);
+    if (!args) {
         goto exit;
     }
+    if (!noptargs) {
+        goto skip_optional_pos;
+    }
+    if (args[0]) {
+        pyyear = args[0];
+        if (!--noptargs) {
+            goto skip_optional_pos;
+        }
+    }
+    if (args[1]) {
+        pymonth = args[1];
+        if (!--noptargs) {
+            goto skip_optional_pos;
+        }
+    }
+    if (args[2]) {
+        pyday = args[2];
+        if (!--noptargs) {
+            goto skip_optional_pos;
+        }
+    }
+    date = args[3];
+skip_optional_pos:
     return_value = cjholiday_holiday_name_impl(module, pyyear, pymonth, pyday, date);
 
 exit:
     return return_value;
 }
-/*[clinic end generated code: output=2675df503a051b9f input=a9049054013a1b77]*/
+/*[clinic end generated code: output=62bb20428d160d9c input=a9049054013a1b77]*/
