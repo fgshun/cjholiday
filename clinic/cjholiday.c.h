d75d6fef8feddc9822e9f9efd4ff992ab9536eac
fgshun <fgshun@gmail.com>
2019-11-03 23:24:43 +0900

    argument clinic on Python 3.6

--- clinic/cjholiday.c.h
+++ clinic/cjholiday.c.h
@@ -3,15 +3,13 @@ preserve
 [clinic start generated code]*/
 
 PyDoc_STRVAR(cjholiday_holiday_name__doc__,
-"holiday_name($module, /, year=<unrepresentable>,\n"
-"             month=<unrepresentable>, day=<unrepresentable>,\n"
-"             date=<unrepresentable>)\n"
+"holiday_name($module, /, year=None, month=None, day=None, date=None)\n"
 "--\n"
 "\n"
 "get name of holiday from ymd or datetime.date.");
 
 #define CJHOLIDAY_HOLIDAY_NAME_METHODDEF    \
-    {"holiday_name", (PyCFunction)(void(*)(void))cjholiday_holiday_name, METH_FASTCALL|METH_KEYWORDS, cjholiday_holiday_name__doc__},
+    {"holiday_name", (PyCFunction)cjholiday_holiday_name, METH_FASTCALL, cjholiday_holiday_name__doc__},
 
 static PyObject *
 cjholiday_holiday_name_impl(PyObject *module, PyObject *pyyear,
@@ -19,48 +17,23 @@ cjholiday_holiday_name_impl(PyObject *module, PyObject *pyyear,
                             PyObject *date);
 
 static PyObject *
-cjholiday_holiday_name(PyObject *module, PyObject *const *args, Py_ssize_t nargs, PyObject *kwnames)
+cjholiday_holiday_name(PyObject *module, PyObject **args, Py_ssize_t nargs, PyObject *kwnames)
 {
     PyObject *return_value = NULL;
     static const char * const _keywords[] = {"year", "month", "day", "date", NULL};
-    static _PyArg_Parser _parser = {NULL, _keywords, "holiday_name", 0};
-    PyObject *argsbuf[4];
-    Py_ssize_t noptargs = nargs + (kwnames ? PyTuple_GET_SIZE(kwnames) : 0) - 0;
+    static _PyArg_Parser _parser = {"|OOOO:holiday_name", _keywords, 0};
     PyObject *pyyear = NULL;
     PyObject *pymonth = NULL;
     PyObject *pyday = NULL;
     PyObject *date = NULL;
 
-    args = _PyArg_UnpackKeywords(args, nargs, NULL, kwnames, &_parser, 0, 4, 0, argsbuf);
-    if (!args) {
+    if (!_PyArg_ParseStack(args, nargs, kwnames, &_parser,
+        &pyyear, &pymonth, &pyday, &date)) {
         goto exit;
     }
-    if (!noptargs) {
-        goto skip_optional_pos;
-    }
-    if (args[0]) {
-        pyyear = args[0];
-        if (!--noptargs) {
-            goto skip_optional_pos;
-        }
-    }
-    if (args[1]) {
-        pymonth = args[1];
-        if (!--noptargs) {
-            goto skip_optional_pos;
-        }
-    }
-    if (args[2]) {
-        pyday = args[2];
-        if (!--noptargs) {
-            goto skip_optional_pos;
-        }
-    }
-    date = args[3];
-skip_optional_pos:
     return_value = cjholiday_holiday_name_impl(module, pyyear, pymonth, pyday, date);
 
 exit:
     return return_value;
 }
-/*[clinic end generated code: output=4719c9686b572051 input=a9049054013a1b77]*/
+/*[clinic end generated code: output=46399c1346e8276a input=a9049054013a1b77]*/
