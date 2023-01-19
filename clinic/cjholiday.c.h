65df4e1569962b3d4ae2ce0cf1e2aba548ae3430
fgshun <fgshun@gmail.com>
2023-01-19 23:44:03 +0900

    argument clinic on Python 3.12.0a4

--- clinic/cjholiday.c.h
+++ clinic/cjholiday.c.h
@@ -2,6 +2,12 @@
 preserve
 [clinic start generated code]*/
 
+#if defined(Py_BUILD_CORE) && !defined(Py_BUILD_CORE_MODULE)
+#  include "pycore_gc.h"            // PyGC_Head
+#  include "pycore_runtime.h"       // _Py_ID()
+#endif
+
+
 PyDoc_STRVAR(cjholiday_holiday_name__doc__,
 "holiday_name($module, /, year=<unrepresentable>,\n"
 "             month=<unrepresentable>, day=<unrepresentable>,\n"
@@ -22,8 +28,31 @@ static PyObject *
 cjholiday_holiday_name(PyObject *module, PyObject *const *args, Py_ssize_t nargs, PyObject *kwnames)
 {
     PyObject *return_value = NULL;
+    #if defined(Py_BUILD_CORE) && !defined(Py_BUILD_CORE_MODULE)
+
+    #define NUM_KEYWORDS 4
+    static struct {
+        PyGC_Head _this_is_not_used;
+        PyObject_VAR_HEAD
+        PyObject *ob_item[NUM_KEYWORDS];
+    } _kwtuple = {
+        .ob_base = PyVarObject_HEAD_INIT(&PyTuple_Type, NUM_KEYWORDS)
+        .ob_item = { &_Py_ID(year), &_Py_ID(month), &_Py_ID(day), &_Py_ID(date), },
+    };
+    #undef NUM_KEYWORDS
+    #define KWTUPLE (&_kwtuple.ob_base.ob_base)
+
+    #else  // !Py_BUILD_CORE
+    #  define KWTUPLE NULL
+    #endif  // !Py_BUILD_CORE
+
     static const char * const _keywords[] = {"year", "month", "day", "date", NULL};
-    static _PyArg_Parser _parser = {NULL, _keywords, "holiday_name", 0};
+    static _PyArg_Parser _parser = {
+        .keywords = _keywords,
+        .fname = "holiday_name",
+        .kwtuple = KWTUPLE,
+    };
+    #undef KWTUPLE
     PyObject *argsbuf[4];
     Py_ssize_t noptargs = nargs + (kwnames ? PyTuple_GET_SIZE(kwnames) : 0) - 0;
     PyObject *pyyear = NULL;
@@ -63,4 +92,4 @@ skip_optional_pos:
 exit:
     return return_value;
 }
-/*[clinic end generated code: output=76a7c6d42cfc64fd input=a9049054013a1b77]*/
+/*[clinic end generated code: output=52d4b8fc0cffa256 input=a9049054013a1b77]*/
